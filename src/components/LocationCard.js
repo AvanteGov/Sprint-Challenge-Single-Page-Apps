bfb9a0466c17462f57be21102ff5cb02ad238978
Alex <alexcouts18@gmail.com>
2020-03-21 12:57:23 -0400

    added locations list rendering for all received data, added styled component stlying to each rendered card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard(props) {
+  
+  console.log(props);
+  
+  const Card = styled.div `
+    width: 80%;
+    margin: 2% auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border: 3px solid lightgrey;
+    border-radius: 25px;
+  `
+  
+  return(
+    <Card className="location-card">
+      <h3 className="location-card__title">{props.location.name}</h3>
+      <span className="location-card__info">{props.location.type}</span>
+      <span className="locaion-card__info">{props.location.dimension}</span>
+    </Card>
+  )
+   
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard";
 
 export default function LocationsList() {
 
+    const [ locationData, setLocationData ] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/location")
+            .then((response) => {
+                // console.log(response.data.results);
+                setLocationData(response.data.results);
+            })
+            .catch((err) =>{
+                console.log("you got an error, Tex", err);
+            })
+    }, [])
+
+
     return(
         <div className="locations-container">
-            <h2> I AM THE LOCATIONS CONTAINER </h2>
+            {locationData.map((location) => {
+                return (
+                    <LocationCard key={location.id} location={location} />
+                )
+            })}
         </div>
     )
 }
